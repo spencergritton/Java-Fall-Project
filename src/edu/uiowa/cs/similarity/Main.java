e9e6033685530c47a703b5ab065aaf9974f7ea5c
SpencerGritton <SpencerGritton@wireless-nat-inside.uiowa.edu>
2017-11-16 16:43:57 -0600

    Added initial line reading, now all lines are in a list

--- src/edu/uiowa/cs/similarity/Main.java
+++ src/edu/uiowa/cs/similarity/Main.java
@@ -3,10 +3,13 @@ package edu.uiowa.cs.similarity;
 import org.apache.commons.cli.*;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Options options = new Options();
         options.addRequiredOption("f", "file", true, "input file to process");
         options.addOption("h", false, "print this help message");
@@ -32,6 +35,52 @@ public class Main {
             HelpFormatter helpf = new HelpFormatter();
             helpf.printHelp("Main", options, true);
             System.exit(0);
+        }
+        
+        // PART ONE
+        
+        // Initializes file
+        File temp = new File(filename);
+        Scanner sc = new Scanner(temp);
+        
+        // Creates List of sentences from file
+        List<String> sentences = new ArrayList<>();
+        
+        int index = 0;
+        boolean startOfSentence = true;
+        boolean endOfSentence = false;
+        
+        // While there are words in the file
+        while (sc.hasNext()) {
+            String item = sc.next();
+            item = item.toLowerCase();
+            
+            // if item is a end of line turn endofline true
+            if (item.contains(".") || item.contains("!") || item.contains("?")) {
+                // Remove end of line punctuation
+                item = item.replaceAll("[!?.\"]", "");
+                endOfSentence = true;
+            }
+            
+            // if start of sentence add to list
+            if (startOfSentence) {
+                sentences.add(item);
+                startOfSentence = false;
+            }
+            
+            // must not be start of sentence so add to a current
+            else { sentences.set(index, sentences.get(index) + " " + item); }
+            
+            // if it's the end of sentence add to index, make start of sentence true and end of sentence false
+            if (endOfSentence) {
+                endOfSentence = false;
+                startOfSentence = true;
+                index ++;
+            }
+        }
+
+        for (String item: sentences) {
+            System.out.println(item);
         }
     }
 }
