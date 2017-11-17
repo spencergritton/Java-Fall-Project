4b8b6875733a70f7c194736517527ba057a33c0e
home <home@172.17.115.189>
2017-11-17 15:10:43 -0600

    Commit

--- src/edu/uiowa/cs/similarity/Main.java
+++ src/edu/uiowa/cs/similarity/Main.java
@@ -1,6 +1,7 @@
 package edu.uiowa.cs.similarity;
 
 import org.apache.commons.cli.*;
+import opennlp.tools.stemmer.*;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -82,6 +83,33 @@ public class Main {
         for (String item: sentences) {
             item = item.replaceAll("[^a-zA-Z0-9é'\\-\\s]", "");
             System.out.println(item);
+        }
+        
+        //implement PorterStemmer to remove root words
+        PorterStemmer stemmer = new PorterStemmer();
+        
+       
+
+        
+        //remove stop words found in stopwords.txt from sentence
+        String [] roots = new String [] {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are",
+            "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could",
+            "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further",
+            "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers",
+            "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it",
+            "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once",
+            "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's",
+            "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", 
+            "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up",
+            "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's",
+            "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've",
+            "your", "yours", "yourself", "yourselves"};
+        
+        for (String root : roots) {
+            if (sentences.contains(root)) {
+                sentences.remove(root);
+                System.out.println(sentences);
+            }    
         }
     }
 }
